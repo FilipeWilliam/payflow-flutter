86ebbc5382dba3a3196109ca71a3ea08d33bc955
Filipe William <lipe.will@outlook.com>
2022-06-14 11:11:12 -0300

    Inicia a tela de leitura do código de barras

---
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/label_button/label_button.dart';
+
+class BarcodeScannerPage extends StatefulWidget {
+  const BarcodeScannerPage({ Key? key }) : super(key: key);
+
+  @override
+  State<BarcodeScannerPage> createState() => _BarcodeScannerPageState();
+}
+
+class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          'Escaneie o código de barras do boleto.',
+          style: TextStyles.buttonBackground,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.transparent,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Row(
+        children: [
+          Expanded(
+            child: LabelButton(
+              label: 'Inserir código do boleto',
+               onPressed: () {}
+            ),
+          ),
+          Expanded(
+            child: LabelButton(
+              label: 'Adicionar da galeria',
+               onPressed: () {}
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
